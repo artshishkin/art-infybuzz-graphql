a0370c48b05903560154fc74a14fba8a3df2b80c
Art <d.art.shishkin@gmail.com>
2022-06-07 09:50:33 +0300

    14. Setting Up Spring Boot App with GraphQL (#1)

---
+++ graphql-getting-started/src/test/java/net/shyshkin/study/graphql/gettingstarted/GraphQLGettingStartedApplicationTest.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.graphql.gettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQLGettingStartedApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
