e81e66772f2bec975dd5dd5b2ad9b02846541e85
Art <d.art.shishkin@gmail.com>
2022-06-07 16:36:01 +0300

    19.1 GraphQL with Input Data - Implementation (#4 Section 5: GraphQL Query)

--- graphql-getting-started/src/main/java/net/shyshkin/study/graphql/gettingstarted/query/Query.java
+++ graphql-getting-started/src/main/java/net/shyshkin/study/graphql/gettingstarted/query/Query.java
@@ -14,4 +14,7 @@ public class Query implements GraphQLQueryResolver {
         return "Second Query";
     }
 
+    public String fullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
 }
