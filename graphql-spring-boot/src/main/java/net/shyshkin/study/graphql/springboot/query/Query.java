8f4a77449c392ec61efec35def33f36f9868ecbf
Art <d.art.shishkin@gmail.com>
2022-06-07 13:33:36 +0300

    3.2 Module `graphql-spring-boot` with spring-boot-starter-graphql (#3)

---
+++ graphql-spring-boot/src/main/java/net/shyshkin/study/graphql/springboot/query/Query.java
@@ -0,0 +1,19 @@
+package net.shyshkin.study.graphql.springboot.query;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Query {
+
+    @QueryMapping
+    public String firstQuery() {
+        return "First Query";
+    }
+
+    @QueryMapping
+    public String secondQuery() {
+        return "Second Query";
+    }
+
+}
---
+++ graphql-spring-boot/src/test/java/net/shyshkin/study/graphql/springboot/GraphQLSpringBootApplicationTest.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.graphql.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GraphQLSpringBootApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
