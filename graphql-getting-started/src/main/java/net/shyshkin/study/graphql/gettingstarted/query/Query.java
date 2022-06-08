4907089c32fb14fc9946a58b4b866cfc29884e60
Art <d.art.shishkin@gmail.com>
2022-06-08 16:21:24 +0300

    27.2 Student Query (#5)

--- graphql-getting-started/src/main/java/net/shyshkin/study/graphql/gettingstarted/query/Query.java
+++ graphql-getting-started/src/main/java/net/shyshkin/study/graphql/gettingstarted/query/Query.java
@@ -1,11 +1,19 @@
 package net.shyshkin.study.graphql.gettingstarted.query;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.graphql.gettingstarted.entity.Student;
 import net.shyshkin.study.graphql.gettingstarted.request.SampleRequest;
+import net.shyshkin.study.graphql.gettingstarted.response.StudentResponse;
+import net.shyshkin.study.graphql.gettingstarted.service.StudentService;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@RequiredArgsConstructor
 public class Query implements GraphQLQueryResolver {
+
+    private final StudentService studentService;
 
     public String firstQuery() {
         return "First Query";
@@ -22,4 +30,11 @@ public class Query implements GraphQLQueryResolver {
     public String fullNameJson(SampleRequest sampleRequest) {
         return sampleRequest.getFirstName() + " " + sampleRequest.getLastName();
     }
+
+    @Transactional
+    public StudentResponse student(long id) {
+        Student student = studentService.getStudentById(id);
+        return new StudentResponse(student);
+    }
+
 }
