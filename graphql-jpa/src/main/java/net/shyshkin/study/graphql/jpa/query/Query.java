bb2fda2881947f0ce86324a4eed203d6a1335c36
Art <d.art.shishkin@gmail.com>
2022-06-08 10:15:05 +0300

    24.2 GraphQL Query with Response Model Class - Implementation [skip ci] (#5)

--- graphql-jpa/src/main/java/net/shyshkin/study/graphql/jpa/query/Query.java
+++ graphql-jpa/src/main/java/net/shyshkin/study/graphql/jpa/query/Query.java
@@ -1,12 +1,19 @@
 package net.shyshkin.study.graphql.jpa.query;
 
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.graphql.jpa.entity.Student;
 import net.shyshkin.study.graphql.jpa.request.SampleRequest;
+import net.shyshkin.study.graphql.jpa.response.StudentResponse;
+import net.shyshkin.study.graphql.jpa.service.StudentService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class Query {
+
+    private final StudentService studentService;
 
     @QueryMapping
     public String firstQuery() {
@@ -26,5 +33,11 @@ public class Query {
     @QueryMapping
     public String fullNameJson(@Argument SampleRequest sampleRequest) {
         return sampleRequest.getFirstName() + " " + sampleRequest.getLastName();
+    }
+
+    @QueryMapping
+    public StudentResponse student(@Argument long id) {
+        Student student = studentService.getStudentById(id);
+        return new StudentResponse(student);
     }
 }
