80abf23975672067eba1b10cec459f773df00de1
Art <d.art.shishkin@gmail.com>
2022-06-09 10:10:22 +0300

    33. Data Filter with GraphQL Edge [skip ci] (#8 Section 8: GraphQL Edges)

---
+++ graphql-getting-started/src/main/java/net/shyshkin/study/graphql/gettingstarted/filter/SubjectNameFilter.java
@@ -0,0 +1,7 @@
+package net.shyshkin.study.graphql.gettingstarted.filter;
+
+public enum SubjectNameFilter {
+    Java,
+    MySQL,
+    MongoDB
+}
--- graphql-getting-started/src/main/java/net/shyshkin/study/graphql/gettingstarted/resolver/StudentResponseResolver.java
+++ graphql-getting-started/src/main/java/net/shyshkin/study/graphql/gettingstarted/resolver/StudentResponseResolver.java
@@ -3,6 +3,7 @@ package net.shyshkin.study.graphql.gettingstarted.resolver;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import net.shyshkin.study.graphql.gettingstarted.entity.Student;
 import net.shyshkin.study.graphql.gettingstarted.entity.Subject;
+import net.shyshkin.study.graphql.gettingstarted.filter.SubjectNameFilter;
 import net.shyshkin.study.graphql.gettingstarted.response.StudentResponse;
 import net.shyshkin.study.graphql.gettingstarted.response.SubjectResponse;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,13 @@ import java.util.List;
 @Component
 public class StudentResponseResolver implements GraphQLResolver<StudentResponse> {
 
-    public List<SubjectResponse> getLearningSubjects(StudentResponse studentResponse) {
+    public List<SubjectResponse> getLearningSubjects(StudentResponse studentResponse, SubjectNameFilter subjectNameFilter) {
         var learningSubjects = new ArrayList<SubjectResponse>();
         Student student = studentResponse.getStudent();
         if (student.getLearningSubjects() != null) {
             for (Subject subject : student.getLearningSubjects()) {
-                learningSubjects.add(new SubjectResponse(subject));
+                if (subjectNameFilter.name().equalsIgnoreCase(subject.getSubjectName()))
+                    learningSubjects.add(new SubjectResponse(subject));
             }
         }
         return learningSubjects;
