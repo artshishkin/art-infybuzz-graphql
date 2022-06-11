4ca3b3a9278e9fd3d6242c61d07309f13d620a34
Art <d.art.shishkin@gmail.com>
2022-06-11 19:54:22 +0300

    61. GraphQL Client To Call GraphQL Query (#14)

--- graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
+++ graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
@@ -1,13 +1,42 @@
 package net.shyshkin.study.graphql.clientlegacy.service;
 
+import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
+import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
+import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.graphql.clientlegacy.response.StudentResponse;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ClientService {
 
-    public StudentResponse getStudent(Long id){
-        throw new RuntimeException("Not Implemented Yet");
+    private final GraphQLWebClient client;
+
+    public StudentResponse getStudent(Long id) {
+
+        String request = "query{\n" +
+                "  student(id:" + id + ") {\n" +
+                "    id\n" +
+                "    firstName\n" +
+                "    lastName\n" +
+                "    email\n" +
+                "    street\n" +
+                "    city\n" +
+                "    learningSubjects(subjectNameFilters: [All]) {\n" +
+                "      id\n" +
+                "      subjectName\n" +
+                "      marksObtained\n" +
+                "    }\n" +
+                "    fullName\n" +
+                "  }\n" +
+                "}";
+
+        var graphQLRequest = GraphQLRequest.builder()
+                .query(request)
+                .build();
+        return client.post(graphQLRequest)
+                .map(resp -> resp.get("student", StudentResponse.class))
+                .block();
     }
 
 }
