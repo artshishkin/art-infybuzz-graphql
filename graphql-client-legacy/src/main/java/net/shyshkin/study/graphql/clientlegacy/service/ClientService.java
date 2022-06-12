1f0b716694226babdac1ca672e3f81ccf0ca1a61
Art <d.art.shishkin@gmail.com>
2022-06-12 10:34:46 +0300

    14.2 Externalize Requests file name (#14)

--- graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
+++ graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
@@ -4,6 +4,7 @@ import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.graphql.clientlegacy.request.CreateStudentRequest;
 import net.shyshkin.study.graphql.clientlegacy.response.StudentResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -14,15 +15,21 @@ public class ClientService {
 
     private final GraphQLWebClient client;
 
+    @Value("${app.resource.query.get-student:get_student.graphql}")
+    private String getStudentResource;
+
+    @Value("${app.resource.mutation.create-student:create_student.graphql}")
+    private String createStudentResource;
+
     public StudentResponse getStudent(Long id) {
         return client
-                .post("get_student.graphql", Map.of("studentId", id), StudentResponse.class)
+                .post(getStudentResource, Map.of("studentId", id), StudentResponse.class)
                 .block();
     }
 
     public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
         return client
-                .post("create_student.graphql", Map.of("createStReq", createStudentRequest), StudentResponse.class)
+                .post(createStudentResource, Map.of("createStReq", createStudentRequest), StudentResponse.class)
                 .block();
     }
 
