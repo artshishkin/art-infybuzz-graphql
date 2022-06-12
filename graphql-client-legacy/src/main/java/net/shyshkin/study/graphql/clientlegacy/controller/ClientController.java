70e5cde3abef731e399ede27033bf0663b9698a6
Art <d.art.shishkin@gmail.com>
2022-06-12 09:53:26 +0300

    63. GraphQL Client To Call GraphQL Mutation (#14)

--- graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/controller/ClientController.java
+++ graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/controller/ClientController.java
@@ -1,12 +1,11 @@
 package net.shyshkin.study.graphql.clientlegacy.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.graphql.clientlegacy.request.CreateStudentRequest;
 import net.shyshkin.study.graphql.clientlegacy.response.StudentResponse;
 import net.shyshkin.study.graphql.clientlegacy.service.ClientService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/students")
@@ -18,6 +17,12 @@ public class ClientController {
     @GetMapping("/{id}")
     public StudentResponse getStudent(@PathVariable Long id) {
         return clientService.getStudent(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public StudentResponse createStudent(@RequestBody CreateStudentRequest createStudentRequest){
+        return clientService.createStudent(createStudentRequest);
     }
 
 }
--- graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
+++ graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
@@ -3,6 +3,7 @@ package net.shyshkin.study.graphql.clientlegacy.service;
 import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
 import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
 import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.graphql.clientlegacy.request.CreateStudentRequest;
 import net.shyshkin.study.graphql.clientlegacy.response.StudentResponse;
 import org.springframework.stereotype.Service;
 
@@ -39,6 +40,32 @@ public class ClientService {
                 .build();
         return client.post(graphQLRequest)
                 .map(resp -> resp.get("student", StudentResponse.class))
+                .block();
+    }
+
+    public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
+        String request = "mutation createStudent($createStReq: CreateStudentRequest){\n" +
+                "  createStudent(createStudentRequest: $createStReq) {\n" +
+                "    id\n" +
+                "    firstName\n" +
+                "    lastName\n" +
+                "    email\n" +
+                "    street\n" +
+                "    city\n" +
+                "    learningSubjects(subjectNameFilters: [All]) {\n" +
+                "      id\n" +
+                "      subjectName\n" +
+                "      marksObtained\n" +
+                "    }\n" +
+                "    fullName\n" +
+                "  }\n" +
+                "}";
+        var graphQLRequest = GraphQLRequest.builder()
+                .query(request)
+                .variables(Map.of("createStReq", createStudentRequest))
+                .build();
+        return client.post(graphQLRequest)
+                .map(resp -> resp.get("createStudent", StudentResponse.class))
                 .block();
     }
 
