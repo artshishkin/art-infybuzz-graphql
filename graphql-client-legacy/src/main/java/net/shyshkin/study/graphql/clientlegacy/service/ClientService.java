90a5ddd69333ea6c133e8f4cdb5cb13680f8521c
Art <d.art.shishkin@gmail.com>
2022-06-12 10:25:56 +0300

    14.1 Requests from graphql files (#14)

--- graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
+++ graphql-client-legacy/src/main/java/net/shyshkin/study/graphql/clientlegacy/service/ClientService.java
@@ -1,6 +1,5 @@
 package net.shyshkin.study.graphql.clientlegacy.service;
 
-import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
 import graphql.kickstart.spring.webclient.boot.GraphQLWebClient;
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.graphql.clientlegacy.request.CreateStudentRequest;
@@ -16,56 +15,14 @@ public class ClientService {
     private final GraphQLWebClient client;
 
     public StudentResponse getStudent(Long id) {
-
-        String request = "query student($studentId: Long){\n" +
-                "  student(id: $studentId) {\n" +
-                "    id\n" +
-                "    firstName\n" +
-                "    lastName\n" +
-                "    email\n" +
-                "    street\n" +
-                "    city\n" +
-                "    learningSubjects(subjectNameFilters: [All]) {\n" +
-                "      id\n" +
-                "      subjectName\n" +
-                "      marksObtained\n" +
-                "    }\n" +
-                "    fullName\n" +
-                "  }\n" +
-                "}";
-
-        var graphQLRequest = GraphQLRequest.builder()
-                .query(request)
-                .variables(Map.of("studentId", id))
-                .build();
-        return client.post(graphQLRequest)
-                .map(resp -> resp.get("student", StudentResponse.class))
+        return client
+                .post("get_student.graphql", Map.of("studentId", id), StudentResponse.class)
                 .block();
     }
 
     public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
-        String request = "mutation createStudent($createStReq: CreateStudentRequest){\n" +
-                "  createStudent(createStudentRequest: $createStReq) {\n" +
-                "    id\n" +
-                "    firstName\n" +
-                "    lastName\n" +
-                "    email\n" +
-                "    street\n" +
-                "    city\n" +
-                "    learningSubjects(subjectNameFilters: [All]) {\n" +
-                "      id\n" +
-                "      subjectName\n" +
-                "      marksObtained\n" +
-                "    }\n" +
-                "    fullName\n" +
-                "  }\n" +
-                "}";
-        var graphQLRequest = GraphQLRequest.builder()
-                .query(request)
-                .variables(Map.of("createStReq", createStudentRequest))
-                .build();
-        return client.post(graphQLRequest)
-                .map(resp -> resp.get("createStudent", StudentResponse.class))
+        return client
+                .post("create_student.graphql", Map.of("createStReq", createStudentRequest), StudentResponse.class)
                 .block();
     }
 
